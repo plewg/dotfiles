63910c68e02cbe4d4db33bf7acdf40e78f70c7a1
Paige Dwight <85356912+plewg@users.noreply.github.com>
2025-04-01 13:04:50 -0400

    [main] set machine, switch to using all plugins from nk-plugins

--- install.sh
+++ install.sh
@@ -8,11 +8,24 @@ repo_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 # go to repo directory
 cd "$repo_dir"
 
+# args
+declare machine="$1"
+if [[ -z "$machine" ]]; then
+    echo 'missing required argument: <machine>' >&2
+    echo '  - laptop-paige' >&2
+    echo '  - work-paige' >&2
+    exit 1
+fi
+
 # install nk
 curl -fsSL 'https://raw.githubusercontent.com/ciiqr/nk/HEAD/install.sh' | bash
 
 # add to path
 export PATH="${HOME}/.nk/bin:${PATH}"
+
+# configure machine
+echo '==> configure machine'
+nk var set machine "$machine"
 
 # provision
 echo '==> provision'
