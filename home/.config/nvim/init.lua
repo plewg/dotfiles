267316539df57a519d5038e4ba2deae9ea24462c
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-01-27 12:40:47 -0500

    [main] use generic autocmd for formatting

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -105,23 +105,30 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
-        null_ls.builtins.formatting.prisma_format,
+        -- null_ls.builtins.formatting.prisma_format,
         null_ls.builtins.formatting.prettier.with({
             prefer_local = "node_modules/.bin",
             extra_filetypes = { "sh" },
         }),
     },
-    on_attach = function(client, bufnr)
-        if client:supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format()
-                end,
-            })
-        end
+    -- on_attach = function(client, bufnr)
+    --     if client:supports_method("textDocument/formatting") then
+    --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --         vim.api.nvim_create_autocmd("BufWritePre", {
+    --             group = augroup,
+    --             buffer = bufnr,
+    --             callback = function()
+    --                 vim.lsp.buf.format()
+    --             end,
+    --         })
+    --     end
+    -- end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*" },
+    callback = function()
+        vim.lsp.buf.format()
     end,
 })
 
