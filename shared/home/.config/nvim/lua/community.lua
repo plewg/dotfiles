fa1a462c1c5c8e1b45b0e7ac3027e2b9d966bbad
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-08-23 17:27:26 -0400

    [main] only use mini.completion for signature help

--- shared/home/.config/nvim/lua/community.lua
+++ shared/home/.config/nvim/lua/community.lua
@@ -18,8 +18,7 @@ return {
     { import = "astrocommunity.motion.harpoon" },
     { import = "astrocommunity.ai.codecompanion-nvim" },
     { import = "astrocommunity.editing-support.conform-nvim" },
-    { import = "astrocommunity.lsp.lsp-signature-nvim" },
+    -- { import = "astrocommunity.lsp.lsp-signature-nvim" },
     { import = "astrocommunity.completion.mini-completion" },
     { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
-    -- import/override with your plugins folder
 }
