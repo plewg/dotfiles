57d5773601fdb7100b9a944e1b57e43ceeca0390
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-08-25 21:13:54 -0400

    [main] don't clobber me bro

--- shared/home/.config/nvim/lua/plugins/astrocore.lua
+++ shared/home/.config/nvim/lua/plugins/astrocore.lua
@@ -11,6 +11,13 @@ return {
         diagnostics = {
             severity_sort = true,
         },
+        sessions = {
+            -- Only save the session if nvim was started with no args
+            -- AAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+            autosave = {
+                cwd = vim.fn.argc(-1) == 0,
+            },
+        },
         options = {
             opt = {
                 clipboard = "",
