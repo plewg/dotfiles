da0e50274351a5df7b435ee7a2972a9c5e76b618
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-08-28 19:50:25 -0400

    [main] fix tab through completions

--- shared/home/.config/nvim/lua/plugins/astrocore.lua
+++ shared/home/.config/nvim/lua/plugins/astrocore.lua
@@ -38,7 +38,15 @@ return {
                 -- false isn't working to unmap here, so doing a noop instead
                 ["<C-x><C-o>"] = "<Nop>",
                 ["<Tab>"] = {
-                    function() return vim.fn.pumvisible() == 1 and "<CR>" or "<Tab>" end,
+                    function()
+                        if vim.snippet.active({ direction = 1 }) then
+                            return "<Cmd>lua vim.snippet.jump(1)<CR>"
+                        elseif vim.fn.pumvisible() == 1 then
+                            return "<CR>"
+                        else
+                            return "<Tab>"
+                        end
+                    end,
                     expr = true,
                     desc = "Accept completion",
                 },
