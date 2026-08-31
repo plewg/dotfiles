2f21abd4aadafe0f953ce21253208e307d62fb40
Paige Dwight <85356912+plewg@users.noreply.github.com>
2026-08-31 11:17:24 -0400

    [main] turn off prettier for shell

--- home/.config/nvim/lua/plugins/plugins.lua
+++ home/.config/nvim/lua/plugins/plugins.lua
@@ -39,13 +39,6 @@ return {
     {
         "stevearc/conform.nvim",
         opts = function(_, opts)
-            if not opts.formatters_by_ft then opts.formatters_by_ft = {} end
-            for _, filetype in ipairs({
-                "sh",
-            }) do
-                opts.formatters_by_ft[filetype] = { "prettierd" }
-            end
-
             opts.default_format_opts = { lsp_format = "first" }
 
             opts.format_on_save = function(bufnr)
